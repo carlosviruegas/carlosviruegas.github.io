9d285169974a7c6061902e2ab15df76d0dc7f6c2
Carlos Viruegas <carlos.viruegas1122@gmail.com>
2024-07-31 06:42:02 -0400

    deploy

---
+++ precache-manifest.672f0cbc28f4cdb7de3dfa5bc92dee79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5895b3093d267bc16294fd0db79f973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1212d4dae7da53ed5a6",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9958ec79118624d28939",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "b1212d4dae7da53ed5a6",
+    "url": "/static/js/2.e2769d12.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.e2769d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9958ec79118624d28939",
+    "url": "/static/js/main.a5b31ef6.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
